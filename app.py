773698ad97d83c9dd78e1640df2c27467e1a6ae8
V. Pranav <pranavvalasa@gmail.com>
2026-02-12 19:26:20 +0530

    Initial commit: Hospital registration system

--- app.py
+++ app.py
@@ -212,4 +212,4 @@ def init_db():
 if __name__ == '__main__':
     init_db()
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
